98bd0b147c0dbe6e897164b15b3fb9db83462efd
Richard Sprague <rasprague@gmail.com>
2022-10-06 13:58:48 -0700

    add list-pcms.py utility to list available sound cards

---
+++ list-pcms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+import alsaaudio
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--all", type=bool, dest='listall', default=False, const=True, nargs='?')
+args = parser.parse_args()
+
+cards = alsaaudio.pcms()
+for card in cards:
+    if args.listall or card.startswith("default"):
+        print(card)
+    
