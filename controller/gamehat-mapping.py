eae1708e8e9cb8db1901d44f0f766a95962f1582
Richard Sprague <rasprague@gmail.com>
2022-10-11 23:52:10 -0700

    add controller mapping file support add mapping files for piboy and gamehat

---
+++ controller/gamehat-mapping.py
@@ -0,0 +1,37 @@
+controller = {
+    'dpad': False,
+    'axis': True,
+    'axis-threshold': 0.9,
+    'buttons': dict(
+        BUTTON_A = 0,
+        BUTTON_B = 1,
+        BUTTON_X = 3,
+        BUTTON_Y = 4,
+        BUTTON_RT = 6,
+        BUTTON_LT = 7,
+        BUTTON_SELECT = 8,
+        BUTTON_START = 9,
+    ),
+    'mapping': dict(
+        # knobs
+        KNOB_1 = 'BUTTON_Y',
+        KNOB_2 = 'BUTTON_X',
+        KNOB_3 = 'BUTTON_B',
+        KNOB_4 = 'BUTTON_A',
+        KNOB_5 = 'BUTTON_RT',
+        KNOB_MODE_SCENE = 'BUTTON_LT',
+        # shift knobs
+        KNOB_GAIN = 'BUTTON_Y',
+        KNOB_TRIGGER_SOURCE = 'BUTTON_X',
+        KNOB_MIDI_CHANNEL = 'BUTTON_B',
+        # keys
+        KEY_SHIFT = 'BUTTON_SELECT',
+        KEY_SECONDARY = 'BUTTON_START',
+        # more keys (hold secondary button)
+        KEY_OSD = 'BUTTON_Y',
+        KEY_PERSIST = 'BUTTON_X',
+        KEY_SAVE = 'BUTTON_B',
+        KEY_SCREENSHOT = 'BUTTON_A',
+        KEY_TRIGGER = 'BUTTON_RT'
+    )
+}
--- start_python_foreground.sh
+++ start_python_foreground.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
+DEVICE="default"
+RATE=44100
+CONTROLLER_MAPPING="mapping.py"
+
 EYESY_PYTHON_PID=0
 CONTROLLER_OSC_PID=0
 ARGS=$@
+
+function usage()
+{
+    echo "Start Eyesy Python Video  in the foreground"
+    echo "  also start controller-to-osc mapping script"
+    echo "Usage $0"
+    echo "  options:"
+    echo "    [ -d | --device ] <D> - use audio device D, defaults to 'default'"
+    echo "      (see output from list-pcms.py)"
+    echo "    [ -r | --rate ] <R> - use R sample rate, defaults to 44100"
+    echo "    [ -m | --controller-mapping <M>- use controller mapping python file M, defaults is mapping.py"
+    echo "    [ -h | --help ] show this helpful message"
+}
+
+function parseargs()
+{
+    OPTIONS=$(getopt -o d:r:m: --long device:,rate:,controller-mapping:,help -- $ARGS)
+    if [ $? -ne 0 ]; then
+	usage
+	exit 1
+    fi
+
+    eval set -- "$OPTIONS"
+
+    while true; do
+	case "$1" in
+	    -d|--device)
+		DEVICE=$2 ; shift 2 ;;
+	    -r|--rate)
+		RATE=$2 ; shift 2 ;;
+	    -m|--controller-mapping)
+		CONTROLLER_MAPPING=$2 ; shift 2 ;;
+	    -h|--help)
+		usage ; shift ; exit 0 ;;
+	    --)
+		shift ; break ;;
+	esac
+    done
+}
 
 function startup()
 {
@@ -10,13 +53,13 @@ function startup()
     sudo systemctl start eyesy-web-socket.service
     sudo systemctl start eyesy-pd.service
 
-    cd /home/pi/Eyesy
-    ./controller-osc.py &
+    cd /home/pi/Eyesy/controller
+    ./controller-osc.py $CONTROLLER_MAPPING &
     CONTROLLER_OSC_PID=$!
     echo "CONTROLLER_OSC_PID=$CONTROLLER_OSC_PID"
 
     cd /home/pi/Eyesy/engines/python
-    python -u main.py $ARGS &
+    python -u main.py -device $DEVICE -rate $RATE &
     EYESY_PYTHON_PID=$!
     echo "EYESY_PYTHON_PID=$EYESY_PYTHON_PID"
 
@@ -33,7 +76,9 @@ function cleanup()
 }
 
 trap cleanup EXIT
+trap cleanup KILL
 exec > >(tee /tmp/video.log) 2>&1
+parseargs
 startup
 while ps -p $EYESY_PYTHON_PID > /dev/null; do
     sleep 1
