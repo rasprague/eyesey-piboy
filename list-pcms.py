be38a5d00eee825b67161b4524e9adb86d91570d
Richard "spike" Sprague <rasprague@gmail.com>
2022-10-18 23:04:19 -0700

    Update lsit-pcms.py to show audio capture devices

--- list-pcms.py
+++ list-pcms.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--all", type=bool, dest='listall', default=False, const=True, nargs='?')
 args = parser.parse_args()
 
-cards = alsaaudio.pcms()
+cards = alsaaudio.pcms(alsaaudio.PCM_CAPTURE)
 for card in cards:
     if args.listall or card.startswith("default"):
         print(card)
