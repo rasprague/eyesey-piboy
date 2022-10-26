fad8f51122de995508ed9e9dc1aa001eb6753567
Richard Sprague <rasprague@gmail.com>
2022-10-25 18:21:30 -0700

    Add RaspPi example startup scripts Updaate other example startup scripts

--- eyesy-gamehat-example.sh
+++ eyesy-gamehat-example.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DEVICE="default"
 RATE=44100
+DOUBLEBUF=1
 CONTROLLER_MAPPING="gamehat-mapping.py"
 
 pushd /home/pi/Eyesy
-./start_python_foreground.sh --device $DEVICE --rate $RATE --controller-mapping $CONTROLLER_MAPPING
+./start_python_foreground.sh --device $DEVICE --rate $RATE --double-buffer $DOUBLEBUF --controller-mapping $CONTROLLER_MAPPING
 popd
